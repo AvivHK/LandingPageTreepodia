96db9ebb7c28e91d5a09d8da207c382b1e5bdaf6
AvivHK <Avivhkn@gmail.com>
2020-08-12 20:16:13 +0300

    done!

--- server.js
+++ server.js
@@ -24,26 +24,26 @@ var transporter = nodemailer.createTransport({
 app.post("/sendmail", (req, res) => {
   console.log("request came");
   let user = req.body;
-  // var mailOptions1 = {
-  //   from: 'avivhh@gmail.com',
-  //   to: 'tal@treepodia.com',
-  //   subject: "A new contact detail arrived!",
-  //   text: `contact name: ${user.firstName} contact last name: ${user.familyName} company: ${user.company}, website: ${user.website}, industry: ${user.industry} ${user.industryOther ? "-" + user.industryOther : ""}, email: ${user.email}, phone: ${user.phone}`
-  // };
-  // var mailOptions2 = {
-  //   from: 'avivhh@gmail.com',
-  //   to: 'ronr@treepodia.com',
-  //   subject: "A new contact detail arrived!",
-  //   text: `contact name: ${user.firstName} contact last name: ${user.familyName} company: ${user.company}, website: ${user.website}, industry: ${user.industry} ${user.industryOther ? "-" + user.industryOther : ""}, email: ${user.email}, phone: ${user.phone}`
-  // };
+  var mailOptions1 = {
+    from: 'avivhh@gmail.com',
+    to: 'tal@treepodia.com',
+    subject: "A new contact detail arrived!",
+    text: `contact name: ${user.firstName} contact last name: ${user.familyName} company: ${user.company}, website: ${user.website}, industry: ${user.industry} ${user.industryOther ? "-" + user.industryOther : ""}, email: ${user.email}, phone: ${user.phone}`
+  };
+  var mailOptions2 = {
+    from: 'avivhh@gmail.com',
+    to: 'ronr@treepodia.com',
+    subject: "A new contact detail arrived!",
+    text: `contact name: ${user.firstName} contact last name: ${user.familyName} company: ${user.company}, website: ${user.website}, industry: ${user.industry} ${user.industryOther ? "-" + user.industryOther : ""}, email: ${user.email}, phone: ${user.phone}`
+  };
   var mailOptions3 = {
     from: 'avivhh@gmail.com',
     to: 'avivhkn@gmail.com',
     subject: "A new contact detail arrived!",
     text: `contact name: ${user.firstName} contact last name: ${user.familyName} company: ${user.company}, website: ${user.website}, industry: ${user.industry} ${user.industryOther ? "-" + user.industryOther : ""}, email: ${user.email}, phone: ${user.phone}`
   };
-  // transporter.sendMail(mailOptions2, function (error, info) {
-  // transporter.sendMail(mailOptions1, function (error, info) {
+  transporter.sendMail(mailOptions2, function (error, info) {
+  transporter.sendMail(mailOptions1, function (error, info) {
   transporter.sendMail(mailOptions3, function (error, info) {
     if (error) {
       console.log(error);
